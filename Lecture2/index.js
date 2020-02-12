f975a9030998848a3d3a61dcfb6c9d3eb8ff0412
ankur-srivastava <ankurlkw@gmail.com>
2020-02-12 22:05:00 +0530

    Lecture2

---
+++ Lecture2/index.js
@@ -0,0 +1,48 @@
+// If you have done any programming you will be at home with variables.
+// If you haven't then don't worry it's quite easy to understand.
+// Variables are used to store values. Value can be a string, a number or other forms of data.
+// Let's understand this with an example. You may have seen websites that ask you to enter your first name etc.
+// Now to store that name we need a variable.
+
+let firstName = 'Ankur';
+
+// Step 1 : Declare a Variable
+// To use variables you need to declare them.
+// We use keywords like let, var or const to declare variables.
+// const is used to declare variables whose values do not change.
+
+let age = 30;
+const color = 'red';
+
+// If we just write
+let price;
+console.log(price);
+
+// It gives undefined
+
+// Initialize a Variable
+// Initialize means assigning a value to a variable.
+let cost = 500;
+
+// Variable Naming Rules
+//
+//   1. Don't use numbers at the start of variables. This is not allowed.
+//   2. Variable names are case sensitive.
+//   3. Using camelCase is safe and recommended.
+//   4. Variable names should be intuitive.
+//   5. Don't use the reserved words. That is words that are part of JS syntax. Like let, const, function etc.
+
+
+// Variable Types
+//
+//   You don't need to specify variable types in JS. It's a dynamically typed language, quite similar to Python.
+//   In other languages like C or Java you may have seen various data types being used like int, long, boolean, float etc. Not needed with JS.
+//
+//   You can store numbers, strings, booleans, arrays and objects.
+//
+//   Let's see some examples:
+
+let message = 'Hello all';
+let age=32;
+let flag = false;
+let a = [10, 12, 32]
